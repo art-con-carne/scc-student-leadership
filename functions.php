11ad06834d26676174d30f69d810cfeb5476258a
Reina Kawakita <ReinaKawakita@Reinas-MacBook-Air.local>
2014-10-30 22:47:30 -0700

    Add custom menus function
    I added 2 functions to register and add the menu

--- functions.php
+++ functions.php
@@ -7,3 +7,24 @@ function sccslResources(){
 }
 
 add_action('wp_enqueue_scripts', 'sccslResources');
+
+//Register custom menus
+function register_my_menus() {
+	register_nav_menus(
+		array( 
+		  'main-menu' => __( 'Main Menu' )
+		)	
+	);
+
+  add_action( 'init', 'register_my_menus' );
+
+}
+
+/* Add custom menus to Dashboard > Menus */       
+if ( function_exists( 'register_nav_menus' ) ) {
+	register_nav_menus(
+		array(
+		  'main-menu' => 'Main Menu'
+		)
+	);
+}
