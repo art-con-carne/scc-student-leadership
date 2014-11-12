0d65509ee1e5a82d33c1b1328999cc239edc1c1c
mikec <citizencody@gmail.com>
2014-11-11 22:45:01 -0800

    SCC footer
    working with the Seattle Central footer - removed school html except for
    footer logo until we can confer w/ thomas

--- footer.php
+++ footer.php
@@ -15,69 +15,12 @@
 					</nav>
 				</div><!--#footer-center-->
 			</div><!--#sl-footer-->
-			<div id="scc-footer">
-			<!-- SCC footer goes here-->
-				    <div class="row">
-					<div class="large-12 small-12 columns top-padding">
-					   <p class="social-media">
-					   <a href="http://www.facebook.com/pages/Seattle-Central-Community-College/57629543563" target="_blank"><span class="icon-2x icon-facebook"></span></a>&nbsp;
-					   <a href="http://twitter.com/SeattleCentral" target="_blank"><span class="icon-2x icon-twitter"></span></a>&nbsp;
-					   <a href="http://www.flickr.com/photos/seattlecentral/" target="_blank"><span class="icon-2x icon-flickr"></span></a>&nbsp;
-					   <a href="http://instagram.com/seattlecentral" target="_blank"><span class="icon-2x icon-instagram"></span></a>
-					   </p> 
-					</div>
-				 </div> 
-				 
-				 <div class="row">
-					<div class="large-12 small-12 columns text-center broadway-campus">
-					   <h4>Seattle Central College</h4>
-					   <p class="smallprint"><em>1701 Broadway Seattle, WA 98122</em><br class="rm-sm-break"> <span class="hide-for-small">|</span> <em>206.934.3800</em> | <em>206.934.4395 TTY</em></p>
-					</div>
-				 </div>     
-				 
-				 <div class="row">
-					<div class="large-12 small-12 columns">
-					   <div id="donate-contact-dis">
-						<ul>
-							<li><a href="http://www.seattlecentral.edu/generalinfo/contact.php"><span class="icon-envelope-alt"></span>Contact</a></li>
-							<li><a href="http://donate.seattlecentral.edu"><span class="icon-money"></span>Donate</a></li>
-							<li><a href="http://www.seattlecolleges.edu/DISTRICT/emergencies/seattlecollegesalerts.aspx"><span class="icon-warning-sign"></span>Emergency Alerts</a></li>
-						 </ul>
-						</div>
-					</div>    
-				  </div>  
-				  
-				  <div class="row">
-					<div class="large-12 small-12 columns top-padding">
-					   <p class="tinyprint hide-for-small text-center">
-					   <a href="javascript:newWin('http://sccdweb.sccd.ctc.edu/Common_files/privacy/privacyCIS.html',500,300)">Privacy</a>&nbsp;|&nbsp;
-					   <a href="http://www.seattlecentral.edu/policy/index.php" target="_blank">Policies</a>&nbsp;|&nbsp;
-					   <a href="http://www.seattlecentral.edu/policy/nondisc.php" target="_blank">Equal Opportunity Institution</a>&nbsp;|&nbsp;
-					   <a href="http://www.seattlecentral.edu/disability-support/index.php" target="_blank">Disability Resources</a>&nbsp;|&nbsp;
-					   <a href="javascript:newWin('/about/disclaimer.html',600,550)">Disclaimer</a>
-					  </p>
-					</div>    
-				  </div>  
-					
-
-				<div class="row">
-					<div class="large-12 small-12 columns">
-						 <p class="college-links tinyprint">
-						THE SEATTLE COLLEGES<br />
-						<a href="http://www.seattlecolleges.com" target="_blank">District</a>&nbsp;|&nbsp;
-						<a href="http://northseattle.edu" target="_blank">North</a>&nbsp;|&nbsp;
-						<a href="http://southseattle.edu" target="_blank">South</a>&nbsp;|&nbsp;
-						<a href="http://www.seattlecentral.edu/index.php">Central</a>&nbsp;|&nbsp;
-						<a href="http://sviweb.sccd.ctc.edu" target="_blank">SVI</a>
-						</p>
-					</div> 
-				</div>
+			<div id="scc-footer"><!-- SCC footer goes here-->
+				    
 				
-				<div class="row">
-					<div class="large-12 small-12 columns top-padding">
-					   <img class="footer-logo" src="<?php bloginfo('template_directory');?>/images/SCC/seattle-central-college-stack.svg" alt="SCC Logo/>
-					</div>   
-				</div>
+				
+					<img src="<?php bloginfo('template_directory');?>/images/SCC/seattle-central-college-stack.svg" alt="SCC Logo"/>
+			
 
 			
 			
