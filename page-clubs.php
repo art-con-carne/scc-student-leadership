594d90dbf5a345a3699410f9c7eca92c52d3faf6
mikec <citizencody@gmail.com>
2014-11-19 23:49:22 -0800

    Fixed page-clubs
    Accidentally deleted a php tag, which was breaking stuff.  Still can't
    seem to get other categories out of the loop though...

--- page-clubs.php
+++ page-clubs.php
@@ -8,13 +8,13 @@
 <div id="main">
     <div id="content">
         <?php
-			$clubs = array('category' => 'clubs');
-			$query = new WP_Query( $clubs );
+			$clublist = array('category' => 'clubs');
+			$query = new WP_Query( $clublist );
 		?>
 	
 		<?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
             <div id="post-<?php the_ID(); ?>">
-                <h2>?php the_title(); ?></h2>     
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h2>     
                 <?php the_content('<p class="serif">More &raquo;</p>'); ?>
                 <?php wp_link_pages(); ?>
             </div><!--/post-->
@@ -23,7 +23,8 @@
 					else:?>
 				Sorry, no posts to be found.
 			<?php endif; ?>
-			
+			<?php wp_reset_postdata(); ?>
+
     </div><!--/content-->
 
 <!-- START SIDEBAR -->
