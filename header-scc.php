aa44440a10f003d1521f3a9ec31fdc120fa129db
mikec <citizencody@gmail.com>
2014-11-20 19:51:57 -0800

    header-scc path change
    yup

--- header-scc.php
+++ header-scc.php
@@ -1,7 +1,7 @@
 <header class="full-width-branded">
 
 <div class="row" id="sccc-header">
-    <a href="http://www.seattlecentral.edu"><div class="header-logo-crop"><img src="/inc/images/seattle-central-college-logo.svg" alt="seattle college logo" class="header-logo"></div></a>
+    <a href="http://www.seattlecentral.edu"><div class="header-logo-crop"><img src="/images/seattle-central-college-logo.svg" alt="seattle college logo" class="header-logo"></div></a>
 
 </div> <!-- end row --
 
