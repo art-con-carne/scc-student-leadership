bdbcadd412291dd23b1a0dcdee7704c7a73e5eae
mikec <citizencody@gmail.com>
2014-11-30 18:28:38 -0800

    single.php
    more changes

--- single.php
+++ single.php
@@ -7,10 +7,14 @@
 <!-- START CONTENT -->
 <div id="main">
     <div id="content">
- 			
+ 			<?php global $post; ?>
+			<h2><?php echo $post->post_title;?></h2>
+			<p><?php echo $post->post_content;?></p>
 			
 			<?php 
 			$slug = get_post_meta($post->ID, "club", true);
+			if (!$slug) $slug = get_post_meta($post->ID, "committee", true);
+			if (!$slug) $slug = get_post_meta($post->ID, "board", true);
 				
 				$query = new WP_Query(
 					array(
@@ -22,8 +26,8 @@
 			<?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
 				 
                 <div id="post-<?php the_ID(); ?>">
-				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-				<?php the_content('<p class="serif">More &raquo;</p>'); ?>
+				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+				<?php the_excerpt('<p class="serif">More &raquo;</p>'); ?>
 			
             </div><!--/post-->
    
