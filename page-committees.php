3a73594418e479abf862cd05b2c9fc3f9f24b847
mikec <citizencody@gmail.com>
2014-11-30 18:41:04 -0800

    org page headings
    changed from h2 to h3

--- page-committees.php
+++ page-committees.php
@@ -14,7 +14,7 @@
 	
 		<?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
             <div id="post-<?php the_ID(); ?>">
-                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>     
+                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>     
             </div><!--/post-->
    
             <?php endwhile; 
