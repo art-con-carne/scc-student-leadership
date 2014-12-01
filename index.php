83d0683a86d9159d64ef00ef9b3b944ebfb381f2
mikec <citizencody@gmail.com>
2014-11-30 22:10:17 -0800

    changes to sidebar-primary
    made a new, better way to get the boards sidebar logos (just hope it
    works when i get it over to the server)

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <div id="content">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <div id="post-<?php the_ID(); ?>">
-                <h2>?php the_title(); ?></h2>   
+                <h2><?php the_title(); ?></h2>   
                 <?php the_content('<p class="serif">More &raquo;</p>'); ?>
                 <?php wp_link_pages(); ?>
             </div><!--/post-->
