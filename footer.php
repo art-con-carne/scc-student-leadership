19cdd6cebb934c535754de55e5539db0f8293d7d
Reina Kawakita <ReinaKawakita@Reinas-MacBook-Air.local>
2014-12-04 20:16:41 -0800

    Added wp_footer() right before closing body tag
    I added this to make the calendar works.

--- footer.php
+++ footer.php
@@ -16,6 +16,12 @@
 		</footer>
 		
 <?php get_template_part("footer", "scc"); ?>
-
+<?php
+   /* Always have wp_footer() just before the closing </body>
+    * tag of your theme, or you will break many plugins, which
+    * generally use this hook to reference JavaScript files.
+    */
+    wp_footer();
+?>
 </body>
 </html>
