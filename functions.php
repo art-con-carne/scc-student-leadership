0bd632392d5cc90290c9e464e497f53331428e0e
jeffreyreynolds <6turtleday@gmail.com>
2014-12-07 13:17:32 -0800

    Misc stylings and breadcrumb code
    Continuing with stylings and added some code for breadcrumbs, but it
    does not seem to work yet.

--- functions.php
+++ functions.php
@@ -91,5 +91,48 @@ function my_register_sidebars() {
 		)
 	);
 	
+	/* Breadcrumbs. Special thanks to the people at Cazue at http://cazue.com/articles/wordpress-creating-breadcrumbs-without-a-plugin-2013  for their excellent code*/
+		function the_breadcrumb() {
+			global $post;
+			echo '<ul id="breadcrumbs">';
+			if (!is_home()) {
+				echo '<li><a href="';
+				echo get_option('home');
+				echo '">';
+				echo 'Home';
+				echo '</a></li><li class="separator"> / </li>';
+				if (is_category() || is_single()) {
+					echo '<li>';
+					the_category(' </li><li class="separator"> / </li><li> ');
+					if (is_single()) {
+						echo '</li><li class="separator"> / </li><li>';
+						the_title();
+						echo '</li>';
+					}
+				} elseif (is_page()) {
+					if($post->post_parent){
+						$anc = get_post_ancestors( $post->ID );
+						$title = get_the_title();
+						foreach ( $anc as $ancestor ) {
+							$output = '<li><a href="'.get_permalink($ancestor).'" title="'.get_the_title($ancestor).'">'.get_the_title($ancestor).'</a></li> <li class="separator">/</li>';
+						}
+						echo $output;
+						echo '<strong title="'.$title.'"> '.$title.'</strong>';
+					} else {
+						echo '<li><strong> '.get_the_title().'</strong></li>';
+					}
+				}
+			}
+			elseif (is_tag()) {single_tag_title();}
+			elseif (is_day()) {echo"<li>Archive for "; the_time('F jS, Y'); echo'</li>';}
+			elseif (is_month()) {echo"<li>Archive for "; the_time('F, Y'); echo'</li>';}
+			elseif (is_year()) {echo"<li>Archive for "; the_time('Y'); echo'</li>';}
+			elseif (is_author()) {echo"<li>Author Archive"; echo'</li>';}
+			elseif (isset($_GET['paged']) && !empty($_GET['paged'])) {echo "<li>Blog Archives"; echo'</li>';}
+			elseif (is_search()) {echo"<li>Search Results"; echo'</li>';}
+			echo '</ul>';
+		}
+
+	
 
 }
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 
 <div id="center">
+	
+
 
 	<div id="main">
 
