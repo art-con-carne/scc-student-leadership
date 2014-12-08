c2ba716d1df3b6a33d5d47791c4481c802001a7b
mikec <citizencody@gmail.com>
2014-12-07 20:40:37 -0800

    single.php minor change
    removing a bit of test text

--- single.php
+++ single.php
@@ -11,7 +11,6 @@
     	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<div id="post-<?php the_ID(); ?>">
 		        <h2><?php the_title(); ?></h2> 
-				<p>And a bit of testing text!</P>
 				<?php the_content('<p class="serif">More &raquo;</p>'); ?>
 			</div>
    
